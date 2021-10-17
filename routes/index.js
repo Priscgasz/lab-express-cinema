1b02dd21462fcbeb1d742ae4686ae66e3a0ecf5d
Priscilla Gasztowtt <pgasztowtt@SIA-Conseil.local>
2021-10-17 23:43:28 +0200

    75% done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const moviesModel = require("./../models/movie.model.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => {
+  res.render("index.hbs");
+});
+
+router.get("/movies", (req, res, next) => {
+  // 1 utiliser un model pour read tous les movies
+  moviesModel.find()
+    .then((movie) => {
+      res.render("movies.hbs", {
+        css: ["movies.css"],
+        movie
+      });
+    })
+    .catch(next);
+  // passer les movies à la view
+  // dans la view {{#each}} pour les movies
+});
+
+router.get("/movies/:id", (req,res,next) => {
+    const id = req.params.id;
+    moviesModel.findById(id)
+    .then((movies) =>{
+        res.render("/oneMovie.hbs", 
+        {movies})
+    })
+    .catch(next)
+})
 
 module.exports = router;
